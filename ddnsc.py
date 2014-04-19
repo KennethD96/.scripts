018e0fc282675222ab277a82ca97f8e64976658a
KennethD96 <Kenneth.dahle.1996@gmail.com>
2014-04-19 23:24:10 +0200

    Added support for Basic HTTP Authentication.

--- ddnsc.py
+++ ddnsc.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python
 #encoding: utf-8
 import urllib2
+import base64
 import socket
 import sys
 ########################
 
 '''Fill in the URL used to update DDNS. Add "<address>" if IP-address is included in the URL.'''
-#ddns_update_url = "http://dyn.dns.he.net/nic/update?hostname=<userID>&myip=<address>"
 ddns_update_url = {
-	6: "http://freedns.afraid.org/dynamic/update.php?ABCD1&address=<address>", # IPv6
-	4: "http://freedns.afraid.org/dynamic/update.php?ABCD2&address=<address>", # IPv4
+	6: "http://freedns.afraid.org/dynamic/update.php?ABC123&address=<address>", # IPv6
+	4: "http://freedns.afraid.org/dynamic/update.php?ABC123&address=<address>", # IPv4
 }
 
 '''Fill HTTP login if DDNS provider uses HTTP for authentication (HE.net is known to do this) or "None" if not.'''
 ddns_http_auth = {
-	"login":	"1337",
-	"password":	"test123",
-	"realm":	"",
+	"login":	"<userid>",
+	"password":	"<password>",
 }
 
 '''Should I update only IPv6, only IPv4 or both?
-Valid options: "v6", "v4", "Both" '''
+Valid options: "v6", "v4", "both" or "auto" if the provider use auto-detect.'''
 mode = "both"
 
 '''Should I send your Global or Local address? (IPv4-only)'''
@@ -50,7 +49,7 @@ if mode.lower() == "v6" or mode.lower() == "both":
 	s.connect(('2001:db8::', 0))
 	ddns_addresses.append(s.getsockname()[0])
 	if debug:
-		log_str("Found address: \"%s\"" % s.getsockname()[0])
+		log_str("Found address IPv6: \"%s\"" % s.getsockname()[0])
 if mode.lower() == "v4" or mode.lower() == "both":
 	if address_scope == "Local":
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -60,23 +59,27 @@ if mode.lower() == "v4" or mode.lower() == "both":
 		address = urllib2.urlopen(get_ip4_url).read().strip()
 	ddns_addresses.append(address)
 	if debug:
-		log_str("Found address: \"%s\"" % address)
+		log_str("Found address IPv4: \"%s\"" % address)
 
-for address in ddns_addresses:
-	ip_ver = 6 if ":" in address else 4
-	if "<address>" in ddns_update_url[ip_ver]:
-		ddns_update_url_tmp = ddns_update_url[ip_ver].replace("<address>", address)
+if not mode.lower() == "auto":
+	for address in ddns_addresses:
+		ip_ver = 6 if ":" in address else 4
+		ddns_update_url_tmp = ddns_update_url[ip_ver]
+		if "<userid>" in ddns_update_url[ip_ver].lower():
+			ddns_update_url_tmp = ddns_update_url_tmp.replace("<userid>", ddns_http_auth["login"])
+		if "<address>" in ddns_update_url[ip_ver].lower():
+			ddns_update_url_tmp = ddns_update_url_tmp.replace("<address>", address)
+		if debug:
+			log_str("Using URL: \"%s\"" % ddns_update_url_tmp)
 		try:
-			ddns_response = urllib2.urlopen(ddns_update_url_tmp, address)
+			req = urllib2.Request(ddns_update_url_tmp)
+			ddns_response = urllib2.urlopen(req)
 		except urllib2.HTTPError as err:
-			if err == "HTTP Error 401: Authorization Required":
-				auth_handler = urllib2.HTTPBasicAuthHandler()
-				auth_handler.add_password(
-					realm=ddns_http_auth["realm"],
-					uri=ddns_update_url_tmp,
-					user=ddns_http_auth["login"],
-					passwd=ddns_http_auth["password"])
-				opener = urllib2.build_opener(auth_handler)
-				urllib2.install_opener(opener)
-				ddns_response = urllib2.urlopen(ddns_update_url_tmp, address)
+			log_str("Got error: %s" % err)
+			base64_auth_string = base64.encodestring("%s:%s" % (
+				ddns_http_auth["login"],
+				ddns_http_auth["password"])
+			)
+			req.add_header("Authorization", "Basic %s" % (base64_auth_string))
+			ddns_response = urllib2.urlopen(req)
 		log_str("Attempted update. Got response: \"%s\"" % ddns_response.read().strip("\n"))
